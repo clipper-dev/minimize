a45bcc3321894f6d046feb9b7221efd9aa93c415
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2023-02-23 01:46:45 +0100

    a

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <main>
       <h1>Test 1</h1>
       <h2>Test 2</h2>
-      <h3 className='red'>Red</h3>
+      <h3 className={styles.green}>Red</h3>
       <h3 className='blue'>Blue</h3>
       <h3 className='green'>Green</h3>
     </main>
