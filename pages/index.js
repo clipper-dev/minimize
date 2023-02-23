0fcdbc58c950f45eb22bc6da37df735d893c706f
clipper-dev <96745726+clipper-dev@users.noreply.github.com>
2023-02-23 01:25:25 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Test 1</h1>
+      <h2>Test 2</h2>
+      <h3 className='red'>Red</h3>
+      <h3 className='blue'>Blue</h3>
+      <h3 className='green'>Green</h3>
+    </main>
+  )
+}
